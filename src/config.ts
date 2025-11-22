05c58671f1d614db0dd6b89dfccc1e0ca3f208c8
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-11-23 03:53:33 +0700

    chore(assets): move assets to public directory
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "kiizuha.dev",
-	subtitle: "This is where I share my thoughts and experiences.",
+	subtitle: "Kiizuha's Personal Blog",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "/assets/site/banner-compressed.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/assets/site/avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Muhammad Rizki",
 	bio: "This is where I share my thoughts and experiences.",
 	links: [
