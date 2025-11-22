28a135c32590cd78cb831701ba75f4393ac8c71d
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-11-22 19:04:50 +0700

    feat(tailwind): add daisyui plugins and its config
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-}
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+	darkMode: "class", // allows toggling dark mode manually
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	daisyui: {
+		prefix: "kz-",
+		themes: false,
+		logs: false,
+		styled: false,
+		base: false,
+		utils: true,
+	},
+};
